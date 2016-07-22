208708094310980760d610ab1478cecc9841741a
Quinntyne Brown <quinntynebrown@gmail.com>
2016-07-22 17:01:57 -0400

    client

---
+++ DelegationPokerApp.Web/src/main.ts
@@ -0,0 +1,34 @@
+﻿require("./app/app-properties");
+
+//import * as reducers from "./app/app.reducers";
+
+//import { ContactsRoutes } from "./app/contacts";
+
+
+import { provide, provideRoutePromise, bootstrap } from "angular-rx-ui/src/components/core";
+import { authorizationGuard } from "angular-rx-ui/src/components/routing/authorization-guard";
+
+import { routeChangeSuccessIsAdminReducer } from "angular-rx-ui/src/components/routing/route-change-success-is-admin.reducer";
+
+import { AppComponent } from "./app/app.component";
+import { AdminAppComponent } from "./app/admin-app.component";
+
+const appModule = angular.module("app", [
+    "components",
+    "ui.tinymce",
+
+    "app.appProperties",
+]);
+
+bootstrap(appModule, {
+    api: "api",
+    components: [AppComponent, AdminAppComponent],
+    loginRedirectUrl: "/",
+    html5Mode: true,
+    guards: [authorizationGuard],
+    run: [routeChangeSuccessIsAdminReducer],
+    //reducers: reducers,
+    routes: [
+//        ...ContactsRoutes,
+    ]
+});
